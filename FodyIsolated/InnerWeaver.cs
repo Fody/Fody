8f9470b9796b0da818b8ce95ceeee7a4b3172b25
Simon Cropp <simon.cropp@gmail.com>
2012-11-03 19:56:59 +1100

    remove redundant code

--- FodyIsolated/InnerWeaver.cs
+++ FodyIsolated/InnerWeaver.cs
@@ -1,9 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Security.Permissions;
 
 public class InnerWeaver : MarshalByRefObject, IInnerWeaver
