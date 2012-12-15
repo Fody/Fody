6701d521a51ea581789136408044de7cad803239
Simon Cropp <simon.cropp@gmail.com>
2012-12-16 10:12:25 +1100

    fix to work around mono codebase bug

--- Fody/AssemblyLocation.cs
+++ Fody/AssemblyLocation.cs
@@ -8,7 +8,9 @@ public static class AssemblyLocation
         // in unt tests returns a path like 
         // C:\Users\Simon\AppData\Local\Temp\o2ehfpqw.x01\Fody.Tests\assembly\dl3\0e7cab25\21728d4f_da04cd01\Fody.dll
         // And that path contains only Fody.dll and no other assemblies
-        var location = typeof(AssemblyLocation).Assembly.CodeBase.Replace("file:///", "");
+		var assembly = typeof(AssemblyLocation).Assembly;
+		var location = assembly.CodeBase.Replace("file:", "");
+		location = location.Replace("//", "/").Replace("//", "/");
 
         return Path.GetDirectoryName(location);
     }
