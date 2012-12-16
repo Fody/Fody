1c12cb84f50da851a410f89aa5becb2d9e59b663
Simon Cropp <simon.cropp@gmail.com>
2012-12-16 17:06:47 +1100

    more mono

--- Fody.Tests/SearchDirectories/AddToolsSolutionDirectoryToAddinSearchTests.cs
+++ Fody.Tests/SearchDirectories/AddToolsSolutionDirectoryToAddinSearchTests.cs
@@ -1,4 +1,3 @@
-using System.IO;
 using NUnit.Framework;
 
 [TestFixture]
--- FodyIsolated/ModuleReader.cs
+++ FodyIsolated/ModuleReader.cs
@@ -3,40 +3,36 @@ using Mono.Cecil;
 
 public partial class InnerWeaver
 {
-    public ModuleDefinition ModuleDefinition;
+	public ModuleDefinition ModuleDefinition;
 
-    FileStream GetSymbolReaderProvider(string assemblyPath)
-    {
-        var pdbPath = Path.ChangeExtension(assemblyPath, "pdb");
-        if (File.Exists(pdbPath))
-        {
-            Logger.LogInfo(string.Format("Found debug symbols at '{0}'.", pdbPath));
-            return File.OpenRead(pdbPath);
-        }
-        var mdbPath = assemblyPath + ".mdb";
+	bool GetSymbolReaderProvider(string assemblyPath)
+	{
+		var pdbPath = Path.ChangeExtension(assemblyPath, "pdb");
+		if (File.Exists(pdbPath))
+		{
+			Logger.LogInfo(string.Format("Found debug symbols at '{0}'.", pdbPath));
+			return true;
+		}
+		var mdbPath = assemblyPath + ".mdb";
 
-        if (File.Exists(mdbPath))
-        {
-            Logger.LogInfo(string.Format("Found debug symbols at '{0}'.", mdbPath));
-            return File.OpenRead(mdbPath);
-        }
+		if (File.Exists(mdbPath))
+		{
+			Logger.LogInfo(string.Format("Found debug symbols at '{0}'.", mdbPath));
+			return true;
+		}
 
-        Logger.LogInfo("Found no debug symbols.");
-        return null;
-    }
+		Logger.LogInfo("Found no debug symbols.");
+		return false;
+	}
 
-    public void ReadModule()
-    {
-        using (var symbolStream = GetSymbolReaderProvider(AssemblyFilePath))
-        {
-            var readSymbols = symbolStream != null;
-            var readerParameters = new ReaderParameters
-            {
-                AssemblyResolver = this,
-                ReadSymbols = readSymbols,
-                SymbolStream = symbolStream,
-            };
-            ModuleDefinition = ModuleDefinition.ReadModule(AssemblyFilePath, readerParameters);
-        }
-    }
+	public void ReadModule()
+	{
+		var readSymbols = GetSymbolReaderProvider(AssemblyFilePath);
+		var readerParameters = new ReaderParameters
+			{
+				AssemblyResolver = this,
+				ReadSymbols = readSymbols,
+			};
+		ModuleDefinition = ModuleDefinition.ReadModule(AssemblyFilePath, readerParameters);
+	}
 }
