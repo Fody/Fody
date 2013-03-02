cd513c5f8fb72369a41c197d9314fa5a05e0b185
Simon Cropp <simon.cropp@gmail.com>
2013-03-02 15:27:30 +1100

    common nuget scripts

---
+++ Nuget/install.ps1
@@ -0,0 +1,54 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+function RemoveForceProjectLevelHack($project)
+{
+	$itemToRemove = $project.ProjectItems.Item("Fody_ToBeDeleted.txt")
+	if ($itemToRemove) {
+		$itemToRemove.Delete()
+	}
+}
+
+function Update-FodyConfig($addinName, $project)
+{
+    $fodyWeaversPath = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($project.FullName), "FodyWeavers.xml")
+
+    if (!(Test-Path ($fodyWeaversPath)))
+    {
+        Throw "Could not find FodyWeavers.xml in this project. Please enable Fody for this projet http://visualstudiogallery.msdn.microsoft.com/074a2a26-d034-46f1-8fe1-0da97265eb7a"
+    }   
+
+    $xml = [xml](get-content $fodyWeaversPath)
+
+    $weavers = $xml["Weavers"]
+    $node = $weavers.SelectSingleNode($addinName)
+
+    if (-not $node)
+    {
+        $newNode = $xml.CreateElement($addinName)
+        $weavers.AppendChild($newNode)
+    }
+
+    $xml.Save($fodyWeaversPath)
+}
+
+function Fix-ReferencesCopyLocal($package, $project)
+{
+    $asms = $package.AssemblyReferences | %{$_.Name}
+
+    foreach ($reference in $project.Object.References)
+    {
+        if ($asms -contains $reference.Name + ".dll")
+        {
+            if($reference.CopyLocal -eq $true)
+            {
+                $reference.CopyLocal = $false;
+            }
+        }
+    }
+}
+
+RemoveForceProjectLevelHack $project
+
+Update-FodyConfig $package.Id.Replace(".Fody", "") $project
+
+Fix-ReferencesCopyLocal $package $project
---
+++ Nuget/uninstall.ps1
@@ -0,0 +1,28 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+
+function Update-FodyConfig($addinName, $project)
+{
+    $fodyWeaversPath = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($project.FullName), "FodyWeavers.xml")
+
+    if (!(Test-Path ($fodyWeaversPath)))
+    {
+        return
+    }   
+
+    $xml = [xml](get-content $fodyWeaversPath)
+
+    $weavers = $xml["Weavers"]
+    $node = $weavers.SelectSingleNode($addinName)
+
+    if ($node)
+    {
+        $weavers.RemoveChild($node)
+    }
+
+    $xml.Save($fodyWeaversPath)
+}
+
+
+
+Update-FodyConfig $package.Id.Replace(".Fody", "") $project
