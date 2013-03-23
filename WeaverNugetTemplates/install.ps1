a1aaa75ef7d254b33181d455e4414a67d4ab1aef
Simon Cropp <simon.cropp@gmail.com>
2013-03-23 16:18:28 +1100

    dont need to check for weavers xml in future

--- WeaverNugetTemplates/install.ps1
+++ WeaverNugetTemplates/install.ps1
@@ -15,11 +15,6 @@ function Update-FodyConfig($addinName, $project)
 	
     $fodyWeaversPath = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($project.FullName), "FodyWeavers.xml")
 
-    if (!(Test-Path ($fodyWeaversPath)))
-    {
-        Throw "Could not find FodyWeavers.xml in this project. Please enable Fody for this projet http://visualstudiogallery.msdn.microsoft.com/074a2a26-d034-46f1-8fe1-0da97265eb7a"
-    }   
-
 	$FodyLastProjectPath = $env:FodyLastProjectPath
 	$FodyLastWeaverName = $env:FodyLastWeaverName
 	$FodyLastXmlContents = $env:FodyLastXmlContents
