0cf40d7def8518affe287cfede82605f409397a6
Simon Cropp <simon.cropp@gmail.com>
2013-08-26 22:15:59 +1000

    use newer of mdb/pdb

--- FodyIsolated/SymbolsFinder.cs
+++ FodyIsolated/SymbolsFinder.cs
@@ -13,25 +13,49 @@ public partial class InnerWeaver
     string pdbPath;
 
     void GetSymbolProviders()
-	{
+    {
 		pdbPath = Path.ChangeExtension(AssemblyFilePath, "pdb");
 		if (File.Exists(pdbPath))
 		{
-		    pdbFound = true;
-            debugReaderProvider = new PdbReaderProvider();
-            debugWriterProvider = new PdbWriterProvider();
-			Logger.LogInfo(string.Format("Found debug symbols at '{0}'.", pdbPath));
+            pdbFound = true;
+            Logger.LogInfo(string.Format("Found debug symbols at '{0}'.", pdbPath));
 		}
 
         mdbPath = AssemblyFilePath + ".mdb";
 		if (File.Exists(mdbPath))
 		{
-		    mdbFound = true;
-		    debugReaderProvider = new MdbReaderProvider();
-            debugWriterProvider = new MdbWriterProvider();
-		    Logger.LogInfo(string.Format("Found debug symbols at '{0}'.", mdbPath));
+            mdbFound = true;
+            Logger.LogInfo(string.Format("Found debug symbols at '{0}'.", mdbPath));
+		}
+
+        if (pdbFound && mdbFound)
+        {
+            if (File.GetLastWriteTimeUtc(pdbPath) >= File.GetLastWriteTimeUtc(mdbPath))
+            {
+                mdbFound = false;
+                Logger.LogInfo("Found mdb and pdb debug symbols. Selected pdb (newer).");
+            }
+            else
+            {
+                pdbFound = false;
+                Logger.LogInfo("Found mdb and pdb debug symbols. Selected mdb (newer).");
+            }
+        }
+
+        if (pdbFound)
+		{
+            debugReaderProvider = new PdbReaderProvider();
+            debugWriterProvider = new PdbWriterProvider();
             return;
 		}
+
+        if (mdbFound)
+		{
+		    debugReaderProvider = new MdbReaderProvider();
+            debugWriterProvider = new MdbWriterProvider();
+            return;
+		}
+
 
 		Logger.LogInfo("Found no debug symbols.");
 	}
