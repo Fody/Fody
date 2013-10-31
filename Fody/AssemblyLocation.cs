12e62acd9be9e79befb1d8b5a073fb81324fbf44
krk <keremkat@gmail.com>
2013-10-31 14:59:00 +0200

    AssemblyLocation Debugger.Launch removed.

--- Fody/AssemblyLocation.cs
+++ Fody/AssemblyLocation.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Diagnostics;
 using System.IO;
 
 public static class AssemblyLocation
 {
     public static string CurrentDirectory()
     {
-        Debugger.Launch();
         //Use codebase because location fails for unit tests.
         var assembly = typeof(AssemblyLocation).Assembly;
         var uri = new UriBuilder(string.IsNullOrEmpty(assembly.CodeBase) ? assembly.Location : assembly.CodeBase);
