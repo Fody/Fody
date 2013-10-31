dba5f9cfc9cf3fdd4a993d0e0afdc08a29147f8f
krk <keremkat@gmail.com>
2013-10-31 14:56:55 +0200

    AssemblyLocation file throws when build path contains the unsupported '#' character.

--- Fody/AssemblyLocation.cs
+++ Fody/AssemblyLocation.cs
@@ -1,14 +1,21 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 
 public static class AssemblyLocation
 {
     public static string CurrentDirectory()
     {
+        Debugger.Launch();
         //Use codebase because location fails for unit tests.
-		var assembly = typeof(AssemblyLocation).Assembly;
-		var uri = new UriBuilder(string.IsNullOrEmpty(assembly.CodeBase) ? assembly.Location : assembly.CodeBase);
-		var path = Uri.UnescapeDataString(uri.Path);
+        var assembly = typeof(AssemblyLocation).Assembly;
+        var uri = new UriBuilder(string.IsNullOrEmpty(assembly.CodeBase) ? assembly.Location : assembly.CodeBase);
+        if (uri.ToString().Contains("#"))
+        {
+            throw new NotSupportedException("'#' character in path is not supported while building projects containing Fody.");
+        }
+
+        var path = Uri.UnescapeDataString(uri.Path);
 
         return Path.GetDirectoryName(path);
     }
