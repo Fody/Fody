dc19df54ea8dac694c1c83789300831253d839da
Simon Cropp <simon.cropp@gmail.com>
2014-03-02 12:20:05 +1100

    fix syncomatic

--- Fody.Tests/SyncRepos.cs
+++ Fody.Tests/SyncRepos.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 using Octokit;
 using SyncOMatic;
@@ -13,75 +14,109 @@ public class SyncRepos
     {
         using (var syncer = new Syncer(GetCredentials(),null, ConsoleLogger))
         {
-            var map = new Mapper()
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("AsyncErrorHandler", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("AsyncErrorHandler", "Fody/NugetAssets/uninstall.ps1"))
-                .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Anotar", "install.ps1"))
-                .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Anotar", "uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("AssertMessage", "NuGet/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("AssertMessage", "NuGet/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("BasicFodyAddin", "Nuget/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("BasicFodyAddin", "Nuget/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Caseless", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Caseless", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("EmptyConstructor", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("EmptyConstructor", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("ExtraConstraints", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("ExtraConstraints", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Equals", "NuGet/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Equals", "NuGet/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Fielder", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Fielder", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Freezable", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Freezable", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Immutable", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Immutable", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("InfoOf", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("InfoOf", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Ionad", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Ionad", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Janitor", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Janitor", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("JetBrainsAnnotations", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("JetBrainsAnnotations", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("MethodDecorator", "install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("MethodDecorator", "uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("MethodTimer", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("MethodTimer", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("ModuleInit", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("ModuleInit", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Obsolete", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Obsolete", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("PropertyChanged", "Nuget/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("PropertyChanged", "Nuget/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("PropertyChanging", "Nuget/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("PropertyChanging", "Nuget/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Publicize", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Publicize", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Resourcer", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Resourcer", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Scalpel", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Scalpel", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Stamp", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Stamp", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("ToString", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("ToString", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Validar", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Validar", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Virtuosity", "Fody/NugetAssets/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Virtuosity", "Fody/NugetAssets/uninstall.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Visualize", "Nuget/install.ps1"))
-                //.Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Visualize", "Nuget/uninstall.ps1"))
-                ;
-            var diff = syncer.Diff(map);
-            foreach (var output in  syncer.Sync(diff, SyncOutput.CreatePullRequest))
+            foreach (var map in Mapper())
             {
-                if (!string.IsNullOrEmpty(output))
+                var diff = syncer.Diff(map);
+                foreach (var output in  syncer.Sync(diff, SyncOutput.CreatePullRequest))
                 {
-                    Console.Out.WriteLine("Done " + output);
+                    if (!string.IsNullOrEmpty(output))
+                    {
+                        Console.Out.WriteLine("Done " + output);
+                    }
                 }
             }
         }
+    }
+
+    static IEnumerable<Mapper> Mapper()
+    {
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("AsyncErrorHandler", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("AsyncErrorHandler", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Anotar", "install.ps1"))
+                .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Anotar", "uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("AssertMessage", "NuGet/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("AssertMessage", "NuGet/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("BasicFodyAddin", "NuGet/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("BasicFodyAddin", "NuGet/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Caseless", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Caseless", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("EmptyConstructor", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("EmptyConstructor", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("ExtraConstraints", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("ExtraConstraints", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Equals", "NuGet/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Equals", "NuGet/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Fielder", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Fielder", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Freezable", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Freezable", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Immutable", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Immutable", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("InfoOf", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("InfoOf", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Ionad", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Ionad", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Janitor", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Janitor", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("JetBrainsAnnotations", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("JetBrainsAnnotations", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("MethodDecorator", "install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("MethodDecorator", "uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("MethodTimer", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("MethodTimer", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("ModuleInit", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("ModuleInit", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Obsolete", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Obsolete", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("PropertyChanged", "Nuget/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("PropertyChanged", "Nuget/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("PropertyChanging", "Nuget/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("PropertyChanging", "Nuget/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Publicize", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Publicize", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Resourcer", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Resourcer", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Scalpel", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Scalpel", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Stamp", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Stamp", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("ToString", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("ToString", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Validar", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Validar", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Virtuosity", "Fody/NugetAssets/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Virtuosity", "Fody/NugetAssets/uninstall.ps1"));
+        yield return new Mapper()
+            .Add(SourceBlob("WeaverNugetTemplates/install.ps1"), TargetBlob("Visualize", "Nuget/install.ps1"))
+            .Add(SourceBlob("WeaverNugetTemplates/uninstall.ps1"), TargetBlob("Visualize", "Nuget/uninstall.ps1"));
     }
 
     static Parts TargetBlob(string repo, string nugetInstallPs1)
