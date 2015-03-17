1fa46bb9cd761a543221436f9460a8152c3f2db6
Simon Cropp <simon.cropp@gmail.com>
2015-03-17 20:38:18 +1100

    dont peverify in ncrunch

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Fody")]
 [assembly: AssemblyProduct("Fody")]
-[assembly: AssemblyVersion("1.28.0")]
-[assembly: AssemblyFileVersion("1.28.0")]
+[assembly: AssemblyVersion("1.28.1")]
+[assembly: AssemblyFileVersion("1.28.1")]
