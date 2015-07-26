f8aa08f4c78f88e73b16604042a968006b1e8754
Cameron MacFarland <distantcam@gmail.com>
2015-07-26 20:42:29 +0800

    Prefixing all messages with running weaver. Fixes #185

--- Fody/BuildLogger.cs
+++ Fody/BuildLogger.cs
@@ -22,17 +22,17 @@ public class BuildLogger : MarshalByRefObject, ILogger
 
     public virtual void LogMessage(string message, MessageImportance level)
     {
-        BuildEngine.LogMessageEvent(new BuildMessageEventArgs(GetIndent() + message, "", "Fody", level));
+        BuildEngine.LogMessageEvent(new BuildMessageEventArgs(GetIndent() + PrependMessage(message), "", "Fody", level));
     }
 
     public virtual void LogDebug(string message)
     {
-        BuildEngine.LogMessageEvent(new BuildMessageEventArgs(GetIndent() + message, "", "Fody", DebugMessageImportant));
+        BuildEngine.LogMessageEvent(new BuildMessageEventArgs(GetIndent() + PrependMessage(message), "", "Fody", DebugMessageImportant));
     }
 
     public virtual void LogInfo(string message)
     {
-        BuildEngine.LogMessageEvent(new BuildMessageEventArgs(GetIndent() + message, "", "Fody", InfoMessageImportant));
+        BuildEngine.LogMessageEvent(new BuildMessageEventArgs(GetIndent() + PrependMessage(message), "", "Fody", InfoMessageImportant));
     }
 
     public virtual void LogWarning(string message)
