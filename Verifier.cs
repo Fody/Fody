7ce827d87d5912546fded4c1f59fb771f83e6b47
Simon Cropp <simon.cropp@gmail.com>
2016-07-17 17:55:41 +1000

    add verifier template

---
+++ Verifier.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Utilities;
+using NUnit.Framework;
+
+public static class Verifier
+{
+    public static void Verify(string beforeAssemblyPath, string afterAssemblyPath)
+    {
+        var before = Validate(beforeAssemblyPath);
+        var after = Validate(afterAssemblyPath);
+        var message = $"Failed processing {Path.GetFileName(afterAssemblyPath)}\r\n{after}";
+        Assert.AreEqual(TrimLineNumbers(before), TrimLineNumbers(after), message);
+    }
+
+    static string Validate(string assemblyPath2)
+    {
+        var exePath = GetPathToPeVerify();
+        if (!File.Exists(exePath))
+        {
+            return string.Empty;
+        }
+        var process = Process.Start(new ProcessStartInfo(exePath, "\"" + assemblyPath2 + "\"")
+        {
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        });
+
+        process.WaitForExit(10000);
+        return process.StandardOutput.ReadToEnd().Trim().Replace(assemblyPath2, "");
+    }
+
+    static string GetPathToPeVerify()
+    {
+        return ToolLocationHelper.GetPathToDotNetFrameworkFile("peverify.exe",TargetDotNetFrameworkVersion.VersionLatest);
+    }
+
+    static string TrimLineNumbers(string foo)
+    {
+        return Regex.Replace(foo, @"0x.*]", "");
+    }
+}
