c2f16c83aeb4bda9217203500691a2d02958290d
Simon Cropp <simon.cropp@gmail.com>
2017-05-23 11:48:39 +1000

    2.0.10

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Fody")]
 [assembly: AssemblyProduct("Fody")]
-[assembly: AssemblyVersion("2.0.9")]
-[assembly: AssemblyFileVersion("2.0.9")]
+[assembly: AssemblyVersion("2.0.10")]
+[assembly: AssemblyFileVersion("2.0.10")]
