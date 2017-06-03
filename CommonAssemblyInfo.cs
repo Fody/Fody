fcbc1a2d9e4f64632b35ab7ee7718e3687acb2d7
Simon Cropp <simon.cropp@gmail.com>
2017-06-03 11:16:05 +1000

    bump version to 2.1.0

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Fody")]
 [assembly: AssemblyProduct("Fody")]
-[assembly: AssemblyVersion("2.0.11")]
-[assembly: AssemblyFileVersion("2.0.11")]
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.0")]
