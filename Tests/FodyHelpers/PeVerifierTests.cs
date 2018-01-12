92fadd748469605f33d5b67824fda44888a6211e
Simon Cropp <simon.cropp@gmail.com>
2018-01-12 21:32:17 +1100

    fix locked file in peverify test

--- Tests/FodyHelpers/PeVerifierTests.cs
+++ Tests/FodyHelpers/PeVerifierTests.cs
@@ -16,12 +16,7 @@ public class PeVerifierTests : TestBase
     [Fact]
     public void Should_verify_current_assembly()
     {
-
-#if (NET46)
-        var verify = PeVerifier.Verify(GetAssemblyPath(), System.Linq.Enumerable.Empty<string>(), out var output);
-#else
-        var verify = PeVerifier.Verify(GetAssemblyPath(), GetNetCoreIgnoreCodes(), out var output);
-#endif
+        var verify = PeVerifier.Verify(GetAssemblyPath(), GetIgnoreCodes(), out var output);
         Assert.True(verify);
         Assert.NotNull(output);
         Assert.NotEmpty(output);
@@ -31,27 +26,25 @@ public class PeVerifierTests : TestBase
     public void Same_assembly_should_not_throw()
     {
         var assemblyPath = GetAssemblyPath().ToLowerInvariant();
-        var newAssemblyPath = assemblyPath.Replace(".dll", "2.dll");
+        Directory.CreateDirectory("temp");
+        var newAssemblyPath = Path.GetFullPath("temp/temp.dll");
         File.Copy(assemblyPath, newAssemblyPath, true);
-#if (NET46)
-        PeVerifier.ThrowIfDifferent(assemblyPath, newAssemblyPath);
-#else
         PeVerifier.ThrowIfDifferent(assemblyPath, newAssemblyPath,
-            ignoreCodes: GetNetCoreIgnoreCodes());
-#endif
+            ignoreCodes: GetIgnoreCodes());
         File.Delete(newAssemblyPath);
     }
 
-    private static string[] GetNetCoreIgnoreCodes()
+    static string[] GetIgnoreCodes()
     {
-        return new[] {"0x80070002", "0x80131869"};
+        return new[] { "0x80070002", "0x80131869" };
     }
 
     [Fact]
     public void Invalid_assembly_should_throw()
     {
         var assemblyPath = GetAssemblyPath().ToLowerInvariant();
-        var newAssemblyPath = assemblyPath.Replace(".dll", "2.dll");
+        Directory.CreateDirectory("temp");
+        var newAssemblyPath = Path.GetFullPath("temp/temp.dll");
         File.Copy(assemblyPath, newAssemblyPath, true);
         using (var moduleDefinition = ModuleDefinition.ReadModule(assemblyPath))
         {
