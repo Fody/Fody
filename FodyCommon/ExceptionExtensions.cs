cb73f867f383e9f546c7f3bd550ac45efa1792a7
Tzach Shabtay <tshabtay@summitgrouptech.com>
2018-01-25 13:18:20 -0500

    fix typo

--- FodyCommon/ExceptionExtensions.cs
+++ FodyCommon/ExceptionExtensions.cs
@@ -17,7 +17,7 @@ public static class ExceptionExtensions
 
             stringBuilder.Append("Type:");
             stringBuilder.Append(Environment.NewLine);
-            stringBuilder.Append(exception.Type.ToString());
+            stringBuilder.Append(exception.GetType().ToString());
             stringBuilder.Append(Environment.NewLine);
 
             foreach (var i in exception.Data)
