f7a69ff8ef0e94248ced572634011aa9b1f16784
Lukas Rieger <lr@live.de>
2018-03-09 00:19:06 +0100

    ignore ref-assemblies when searching for InSolution weaver dll. fixes https://github.com/Fody/Fody/issues/491

---
+++ FodyHelpers/ReferenceAssemblyChecker.cs
@@ -0,0 +1,20 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fody
+{
+    public static class ReferenceAssemblyChecker
+    {
+        public static bool IsReferenceAssembly(string path)
+        {
+            var asm = AssemblyDefinition.ReadAssembly(path);
+            var isRefAssembly = asm.CustomAttributes.Any(a => a.AttributeType.FullName == "System.Runtime.CompilerServices.ReferenceAssemblyAttribute");
+            return isRefAssembly;
+        }
+
+        public static bool IsImplementationAssembly(string path) => false == IsReferenceAssembly(path);
+    }
+}
