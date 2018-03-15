5d57c46c33dae0f7266442bbf7d6d61600593d3e
Simon Cropp <simon.cropp@gmail.com>
2018-03-15 20:38:23 +1100

    Update BaseModuleWeaver.cs

--- FodyHelpers/BaseModuleWeaver.cs
+++ FodyHelpers/BaseModuleWeaver.cs
@@ -132,6 +132,7 @@ namespace Fody
         /// Uses all assemblies listed from calling <see cref="GetAssembliesForScanning"/> on all weavers.
         /// </summary>
         public Func<string, TypeDefinition> FindType { get; set; } = _ => throw new WeavingException($"{nameof(FindType)} has not been set.");
+
         /// <summary>
         /// Handler for searching for a type.
         /// Uses all assemblies listed from calling <see cref="GetAssembliesForScanning"/> on all weavers.
@@ -147,7 +148,7 @@ namespace Fody
 
         /// <summary>
         /// Set to true if the reference to an equally named library (same name as this fody addin with the ending ".Fody" trimmed)
-		/// should be removed by Fody after a successful execution
+        /// should be removed by Fody after a successful execution
         /// </summary>
         public virtual bool ShouldCleanReference => false;
     }
