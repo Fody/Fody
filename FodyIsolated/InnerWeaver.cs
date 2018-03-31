debdfd371c2723e67c051d0dc5203b8274c7cb72
Simon Cropp <simon.cropp@gmail.com>
2018-03-31 18:13:59 +1100

    .

--- FodyIsolated/InnerWeaver.cs
+++ FodyIsolated/InnerWeaver.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 #if NET46
 using System.Runtime.Remoting;
 #endif
--- Tests/FodyIsolated/WeaverInitialiserTests.cs
+++ Tests/FodyIsolated/WeaverInitialiserTests.cs
@@ -1,10 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Xml.Linq;
 using Fody;
 using Mono.Cecil;
-using Mono.Cecil.Cil;
 using Moq;
 using Xunit;
 
