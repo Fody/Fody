eb2d47fb09020208f3d1780a2b4903845e679573
tom-englert <mail@tom-englert.de>
2018-07-28 18:22:19 +0200

    Add failing test to reproduce issue #532

--- DummyAssembly/Class1.cs
+++ DummyAssembly/Class1.cs
@@ -2,5 +2,16 @@
 {
     public class Class1
     {
+        private int field;
+
+        public void Method(int param)
+        {
+            field = param;
+            param += 1;
+            if (param == field)
+            {
+                field -= 1;
+            }
+        }
     }
 }
