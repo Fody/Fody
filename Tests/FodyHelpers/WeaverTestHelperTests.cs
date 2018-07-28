bf1277ffe3640cc05fd3e45c9c5478bcfa66a5c1
tom-englert <mail@tom-englert.de>
2018-07-28 19:37:39 +0200

    WeaverTestHelper writes back symbols after weaving.

--- Tests/FodyHelpers/WeaverTestHelperTests.cs
+++ Tests/FodyHelpers/WeaverTestHelperTests.cs
@@ -51,9 +51,13 @@ public class WeaverTestHelperTests : TestBase
         var weaver = new WeaverUsingSymbols();
         var start = DateTime.Now;
         var result = weaver.ExecuteTestRun(assemblyPath);
-        var newSymbolsPath = Path.ChangeExtension(result.AssemblyPath, ".pdb");
+        
+        var symbolsPath = Path.ChangeExtension(result.AssemblyPath, ".pdb");
 
-        Assert.True(start <= new FileInfo(newSymbolsPath).CreationTime);
+        var symbolsFileInfo = new FileInfo(symbolsPath);
+
+        Assert.True(symbolsFileInfo.Exists);
+        Assert.True(start <= symbolsFileInfo.CreationTime);
 
 #if NET46 // TODO: Remove when ObjectApproval supports .NET Core
         ObjectApprover.VerifyWithJson(result, ScrubCurrentDirectory);
