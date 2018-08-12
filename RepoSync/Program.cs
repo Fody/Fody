a6017ede813a1163f6da2ae74cb92a585373959f
Simon Cropp <simon.cropp@gmail.com>
2018-08-12 20:59:54 +1000

    add GitHubSync

---
+++ RepoSync/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using GitHubSync;
+using Octokit;
+
+class Program
+{
+    static Task Main()
+    {
+        var githubToken = Environment.GetEnvironmentVariable("Octokit_OAuthToken");
+
+        var credentials = new Credentials(githubToken);
+        var repoSync = new RepoSync(credentials, "Fody", "Fody", "master", Console.WriteLine);
+        repoSync.AddSourceItem(TreeEntryTargetType.Blob, "RepoSync/appveyor.yml", "appveyor.yml");
+        repoSync.AddTarget("Fody", "Caseless", "master");
+
+        return repoSync.Sync();
+    }
+}
