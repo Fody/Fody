21f611bd52ccb1281e763a09014fca175c759fc5
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2018-09-08 23:26:16 +0200

    Add failing integration test

---
+++ Integration/Tests/WithEmbeddedPdbTest.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+using WithEmbeddedPdb;
+using Xunit;
+
+public class WithEmbeddedPdbTest
+{
+    [Fact]
+    public void EnsureTypeChangedByNugetWeaver()
+    {
+        Assert.True(typeof(Class1).GetMethod("Method").IsVirtual);
+    }
+
+    [Fact]
+    public void EnsureDebugInfoIsPresent()
+    {
+        var filePath = typeof(Class1).Assembly.Location;
+
+        using (var file = File.OpenRead(filePath))
+        using (var peReader = new PEReader(file))
+        {
+            var debugInfo = peReader.ReadDebugDirectory();
+            Assert.Contains(debugInfo, i => i.Type == DebugDirectoryEntryType.EmbeddedPortablePdb);
+
+            var metadataReader = peReader.GetMetadataReader();
+
+            using (var provider = peReader.ReadEmbeddedPortablePdbDebugDirectoryData(debugInfo.Single(i => i.Type == DebugDirectoryEntryType.EmbeddedPortablePdb)))
+            {
+                var debugReader = provider.GetMetadataReader();
+
+                var found = false;
+
+                foreach (var debugInfoHandle in debugReader.MethodDebugInformation)
+                {
+                    var method = metadataReader.GetMethodDefinition(debugInfoHandle.ToDefinitionHandle());
+                    if (metadataReader.GetString(method.Name) != "Method")
+                        continue;
+
+                    var methodDebugInfo = debugReader.GetMethodDebugInformation(debugInfoHandle);
+                    var docHandle = debugReader.GetDocument(methodDebugInfo.Document);
+
+                    var docName = debugReader.GetString(docHandle.Name);
+                    Assert.EndsWith("Class1.cs", docName);
+                    found = true;
+                    break;
+                }
+
+                Assert.True(found);
+            }
+        }
+    }
+}
