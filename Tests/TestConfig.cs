20183c9cd36c134c4d0c09de6e087f338b1b0610
simoncropp <simon.cropp@gmail.com>
2018-10-03 19:49:18 +1000

    add reporter

--- Tests/TestConfig.cs
+++ Tests/TestConfig.cs
@@ -1,3 +1,5 @@
-﻿using Xunit;
+﻿using ApprovalTests.Reporters;
+using Xunit;
 
+[assembly: UseReporter(typeof(ClipboardReporter), typeof(DiffReporter))]
 [assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true)]
