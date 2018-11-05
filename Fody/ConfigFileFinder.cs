fc5476d4e6f3ccafba149889dc516bcdc6ccf6fd
tom-englert <mail@tom-englert.de>
2018-11-05 19:58:12 +0100

    Create a default FodyWeavers.xml including schema

--- Fody/ConfigFileFinder.cs
+++ Fody/ConfigFileFinder.cs
@@ -1,10 +1,17 @@
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
 using Fody;
 
-public class ConfigFileFinder
+public static class ConfigFile
 {
-    public static List<string> FindWeaverConfigs(string solutionDirectoryPath, string projectDirectory, ILogger logger)
+    private static readonly XNamespace schemaNamespace = XNamespace.Get("http://www.w3.org/2001/XMLSchema");
+    private static readonly XNamespace schemaInstanceNamespace = XNamespace.Get("http://www.w3.org/2001/XMLSchema-instance");
+
+    public static List<string> FindWeaverConfigs(string solutionDirectoryPath, string projectDirectory, ILogger logger, IEnumerable<string> wellKnownWeaverFiles)
     {
         var files = new List<string>();
 
@@ -18,16 +25,27 @@ public class ConfigFileFinder
         var projectConfigFilePath = Path.Combine(projectDirectory, "FodyWeavers.xml");
         if (!File.Exists(projectConfigFilePath))
         {
-            logger.LogDebug($@"Could not file a FodyWeavers.xml at the project level ({projectConfigFilePath}). Some project types do not support using NuGet to add content files e.g. netstandard projects. In these cases it is necessary to manually add a FodyWeavers.xml to the project. Example content:
+            if (!files.Any() && wellKnownWeaverFiles != null)
+            {
+                GenerateDefault(projectConfigFilePath, wellKnownWeaverFiles);
+
+                logger.LogError($@"Could not file a FodyWeavers.xml at the project level ({projectConfigFilePath}). A default file has been created. Please review the file and add it to your project.");
+            }
+            else
+            {
+                logger.LogDebug($@"Could not file a FodyWeavers.xml at the project level ({projectConfigFilePath}). Some project types do not support using NuGet to add content files e.g. netstandard projects. In these cases it is necessary to manually add a FodyWeavers.xml to the project. Example content:
   <Weavers>
     <WeaverName/>
   </Weavers>
   ");
+            }
         }
         else
         {
             files.Add(projectConfigFilePath);
             logger.LogDebug($"Found path to weavers file '{projectConfigFilePath}'.");
+
+            EnsureSchemaIsUpToDate(projectConfigFilePath, wellKnownWeaverFiles);
         }
 
         if (files.Count == 0)
@@ -35,6 +53,124 @@ public class ConfigFileFinder
             var pathsSearched = string.Join("', '", solutionConfigFilePath, projectConfigFilePath);
             throw new WeavingException($"Could not find path to weavers file. Searched '{pathsSearched}'.");
         }
+
         return files;
     }
+
+    static void GenerateDefault(string projectConfigFilePath, IEnumerable<string> wellKnownWeaverFiles)
+    {
+        if (wellKnownWeaverFiles == null)
+            return;
+
+        var weaverConfig = new XDocument(new XElement("Weavers", SchemaInstanceAttributes));
+
+        var weaverEntries = wellKnownWeaverFiles
+            .Select(WeaverNameFromFilePath)
+            .Select(name => new XElement(name))
+            .ToArray();
+
+        weaverConfig.Root.Add(weaverEntries);
+        weaverConfig.Save(projectConfigFilePath);
+
+        CreateSchemaForConfig(projectConfigFilePath, wellKnownWeaverFiles);
+    }
+
+    static void CreateSchemaForConfig(string projectConfigFilePath, IEnumerable<string> wellKnownWeaverFiles)
+    {
+        if (wellKnownWeaverFiles == null)
+            return;
+
+        var schema = XDocument.Parse(Fody.Properties.Resources.FodyWeavers_SchemaTemplate);
+
+        var baseNode = schema.Descendants().FirstOrDefault(item => item.Name == schemaNamespace.GetName("all"));
+
+        var fragments = wellKnownWeaverFiles.Select(CreateItemFragment);
+
+        baseNode.Add(fragments);
+
+        var filePath = Path.ChangeExtension(projectConfigFilePath, ".xsd");
+
+        try
+        {
+            if (File.Exists(filePath) && string.Equals(XDocument.Load(filePath).ToString(SaveOptions.OmitDuplicateNamespaces | SaveOptions.DisableFormatting), schema.ToString(SaveOptions.OmitDuplicateNamespaces | SaveOptions.DisableFormatting)))
+            {
+                // don't touch existing file if it is up to date
+                return;
+            }
+        }
+        catch
+        {
+            // invalid xsd, overwrite always...
+        }
+
+        schema.Save(filePath, SaveOptions.OmitDuplicateNamespaces);
+    }
+
+    private static XElement CreateItemFragment(string weaverFile)
+    {
+        var weaverName = WeaverNameFromFilePath(weaverFile);
+
+        var element = new XElement(schemaNamespace.GetName("element"),
+            new XAttribute("name", weaverName),
+            new XAttribute("minOccurs", 0),
+            new XAttribute("maxOccurs", 1));
+
+        var fragmentFileName = Path.ChangeExtension(weaverFile, ".xcf");
+
+        if (File.Exists(fragmentFileName))
+        {
+            try
+            {
+                element.Add(XElement.Parse(File.ReadAllText(fragmentFileName)));
+                return element;
+            }
+            catch
+            {
+                // invalid fragment, ignore...
+            }
+        }
+
+        element.Add(new XAttribute("type", "xs:anyType"));
+
+        return element;
+    }
+
+    private static string WeaverNameFromFilePath(string filePath)
+    {
+        return Path.ChangeExtension(Path.GetFileNameWithoutExtension(filePath), null);
+    }
+
+    static void EnsureSchemaIsUpToDate(string projectConfigFilePath, IEnumerable<string> wellKnownWeaverFiles)
+    {
+        if (wellKnownWeaverFiles == null)
+            return;
+
+        try
+        {
+            var doc = XDocument.Load(projectConfigFilePath);
+
+            var hasNamespace = doc.Root.Attributes()
+                .Any(attr => !attr.IsNamespaceDeclaration && attr.Name.LocalName == "noNamespaceSchemaLocation" && string.Equals(attr.Value, "FodyWeavers.xsd", StringComparison.OrdinalIgnoreCase));
+
+            if (!hasNamespace)
+            {
+                doc.Root.Add(SchemaInstanceAttributes);
+                doc.Save(projectConfigFilePath);
+            }
+
+            CreateSchemaForConfig(projectConfigFilePath, wellKnownWeaverFiles);
+        }
+        catch
+        {
+            // anything wrong with the existing, ignore here, we will warn later... 
+        }
+    }
+
+    static XAttribute[] SchemaInstanceAttributes =>
+
+        new []
+    {
+        new XAttribute(XNamespace.Xmlns + "xsi", schemaInstanceNamespace.NamespaceName),
+        new XAttribute(schemaInstanceNamespace.GetName("noNamespaceSchemaLocation"), "FodyWeavers.xsd"),
+    };
 }
--- SampleTarget/WeaverIntegrationTests.cs
+++ SampleTarget/WeaverIntegrationTests.cs
@@ -8,7 +8,9 @@ using Xunit;
 
 namespace SampleTarget
 {
-    public class UpdateReferenceCopyLocalFilesTests
+    using JetBrains.Annotations;
+
+    public class WeaverIntegrationTests
     {
 #if IS_INLINE_INTEGRATION_TEST_ACTIVE
         private const string skipReason = null;
@@ -20,7 +22,7 @@ namespace SampleTarget
         public void SampleWeaverAddedExtraFileDuringBuild()
         {
             var assemblyPath = new Uri(GetType().Assembly.CodeBase).LocalPath;
-            var targetFolder = Path.GetDirectoryName(assemblyPath);
+            var targetFolder = AppDomain.CurrentDomain.BaseDirectory;
             var extraFilePath = Path.Combine(targetFolder, "SomeExtraFile.txt");
             var extraFileContent = File.ReadAllText(extraFilePath);
             var assemblyBuildTime = File.GetLastWriteTime(assemblyPath);
@@ -33,12 +35,38 @@ namespace SampleTarget
         [Fact(Skip = skipReason)]
         public void SampleWeaverRemovedObsoleteDependenciesDuringBuild()
         {
-            var assemblyPath = new Uri(GetType().Assembly.CodeBase).LocalPath;
-            var targetFolder = Path.GetDirectoryName(assemblyPath);
+            var targetFolder = AppDomain.CurrentDomain.BaseDirectory;
 
             var sampleWeaverFiles = Directory.EnumerateFiles(targetFolder, "SampleWeaver.*");
 
             Assert.Empty(sampleWeaverFiles);
+        }
+
+        [Fact(Skip = skipReason)]
+        public void NullGuardsAreActive()
+        {
+            Assert.Throws<ArgumentNullException>(() => GuardedMethod(null));
+        }
+
+        [Fact(Skip = skipReason)]
+        public void WeaverConfigurationIsRead()
+        {
+            var type = Type.GetType("SampleWeaverTest.Configuration");
+            var content = (string)type.GetField("Content").GetValue(null);
+            const string expectedContent = "<SampleWeaver MyProperty=\"PropertyValue\">\r\n  <Content>Test</Content>\r\n</SampleWeaver>";
+
+            Assert.Equal(expectedContent, content);
+
+            var propertyValue = (string)type.GetField("PropertyValue").GetValue(null);
+            const string expectedPropertyValue = "PropertyValue";
+
+            Assert.Equal(expectedPropertyValue, propertyValue);
+        }
+
+        [NotNull]
+        public object GuardedMethod([NotNull] object parameter)
+        {
+            return parameter;
         }
     }
 }
