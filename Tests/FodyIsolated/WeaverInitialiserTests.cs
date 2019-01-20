726bf79edca73f09976dfabcc6f5069c442f39de
simoncropp <simon.cropp@gmail.com>
2019-01-20 21:19:10 +1100

    simplify test

--- Tests/FodyIsolated/WeaverInitialiserTests.cs
+++ Tests/FodyIsolated/WeaverInitialiserTests.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Fody;
 using Mono.Cecil;
 using Moq;
+using ObjectApproval;
 using Xunit;
 
 public class WeaverInitialiserTests : TestBase
@@ -24,10 +25,15 @@ public class WeaverInitialiserTests : TestBase
         var moduleWeaver = new ValidFromBaseModuleWeaver();
         innerWeaver.SetProperties(weaverEntry, moduleWeaver);
 
-        ValidateProps(moduleWeaver, moduleDefinition);
-        Assert.NotNull(moduleWeaver.FindType);
-        Assert.NotNull(moduleWeaver.TryFindType);
-        Assert.NotNull(moduleWeaver.ResolveAssembly);
+        SerializerBuilder.IgnoreMembersWithType<ModuleDefinition>();
+        ObjectApprover.VerifyWithJson(moduleWeaver);
+        Assert.NotNull(moduleWeaver.LogDebug);
+        Assert.NotNull(moduleWeaver.LogInfo);
+        Assert.NotNull(moduleWeaver.LogWarning);
+        Assert.NotNull(moduleWeaver.LogWarningPoint);
+        Assert.NotNull(moduleWeaver.LogError);
+        Assert.NotNull(moduleWeaver.LogErrorPoint);
+        Assert.NotNull(moduleWeaver.LogMessage);
     }
 
     static InnerWeaver BuildInnerWeaver(ModuleDefinition moduleDefinition, AssemblyResolver resolver)
@@ -56,31 +62,6 @@ public class WeaverInitialiserTests : TestBase
             assemblyResolver = resolver,
             TypeCache = new TypeCache(resolver.Resolve)
         };
-    }
-
-    static void ValidateProps(dynamic moduleWeaver, ModuleDefinition moduleDefinition)
-    {
-        Assert.NotNull(moduleWeaver.LogDebug);
-        Assert.NotNull(moduleWeaver.LogInfo);
-        Assert.NotNull(moduleWeaver.LogWarning);
-        Assert.NotNull(moduleWeaver.LogWarningPoint);
-        Assert.NotNull(moduleWeaver.LogError);
-        Assert.NotNull(moduleWeaver.LogErrorPoint);
-        Assert.NotNull(moduleWeaver.LogMessage);
-        Assert.Equal("Ref1;Ref2", moduleWeaver.References);
-        Assert.Equal("CopyRef1", moduleWeaver.ReferenceCopyLocalPaths[0]);
-        Assert.Equal("CopyRef2", moduleWeaver.ReferenceCopyLocalPaths[1]);
-        Assert.Equal("Debug", moduleWeaver.DefineConstants[0]);
-        Assert.Equal("Release", moduleWeaver.DefineConstants[1]);
-
-        // Assert.IsNotEmpty(moduleWeaver.References);
-        Assert.Equal(moduleDefinition, moduleWeaver.ModuleDefinition);
-        Assert.Equal(@"c:\FakePath", moduleWeaver.AddinDirectoryPath);
-        Assert.Equal("AssemblyFilePath", moduleWeaver.AssemblyFilePath);
-        Assert.Equal("ProjectDirectoryPath", moduleWeaver.ProjectDirectoryPath);
-        Assert.Equal("ProjectFilePath", moduleWeaver.ProjectFilePath);
-        Assert.Equal("SolutionDirectoryPath", moduleWeaver.SolutionDirectoryPath);
-        Assert.Equal("DocumentationFile", moduleWeaver.DocumentationFilePath);
     }
 }
 
