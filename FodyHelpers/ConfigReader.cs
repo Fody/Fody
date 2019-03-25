f254d0eab54ab6be0a31584350bf51c3a61b4a14
simoncropp <simon.cropp@gmail.com>
2019-03-25 22:07:20 +1100

    .

--- FodyHelpers/ConfigReader.cs
+++ FodyHelpers/ConfigReader.cs
@@ -1,41 +1,35 @@
 using System.Xml;
 using System.Xml.Linq;
+using Fody;
 
-namespace Fody
+static class ConfigReader
 {
     /// <summary>
-    /// Helper methods for reading from <see cref="BaseModuleWeaver.Config"/>.
+    /// Read a bool from an attribute named <paramref name="name"/>.
     /// </summary>
-    public static class ConfigReader
+    public static bool ReadBool(this XElement element, string name, bool defaultValue = false)
     {
-
-        /// <summary>
-        /// Read a bool from an attribute named <paramref name="name"/>.
-        /// </summary>
-        public static bool ReadBool(this XElement element, string name, bool defaultValue = false)
+        Guard.AgainstNull(nameof(element), element);
+        Guard.AgainstNullAndEmpty(nameof(name), name);
+        var attribute = element.Attribute(name);
+        if (attribute == null)
         {
-            Guard.AgainstNull(nameof(element), element);
-            Guard.AgainstNullAndEmpty(nameof(name), name);
-            var attribute = element.Attribute(name);
-            if (attribute == null)
-            {
-                return defaultValue;
-            }
+            return defaultValue;
+        }
 
-            var value = attribute.Value;
-            if (string.IsNullOrWhiteSpace(value))
-            {
-                throw new WeavingException($"Could not extract '{name}' from xml config. Null, empty, or whitespace are invalid.");
-            }
+        var value = attribute.Value;
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new WeavingException($"Could not extract '{name}' from xml config. Null, empty, or whitespace are invalid.");
+        }
 
-            try
-            {
-               return XmlConvert.ToBoolean(value);
-            }
-            catch
-            {
-                throw new WeavingException($"Could not extract '{name}' from xml config. Value '{value}' could not be converted to a bool. Only '1', '0', 'true', or 'false' are valid.");
-            }
+        try
+        {
+            return XmlConvert.ToBoolean(value);
+        }
+        catch
+        {
+            throw new WeavingException($"Could not extract '{name}' from xml config. Value '{value}' could not be converted to a bool. Only '1', '0', 'true', or 'false' are valid.");
         }
     }
 }
