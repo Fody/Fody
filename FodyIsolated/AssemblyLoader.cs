9723392216ea536adc61495641b4b899fa42f244
simoncropp <simon.cropp@gmail.com>
2019-03-30 23:10:29 +1100

    move weavers back to portable pdb
    fixes #628

--- FodyIsolated/AssemblyLoader.cs
+++ FodyIsolated/AssemblyLoader.cs
@@ -24,19 +24,6 @@ public partial class InnerWeaver
     static Assembly LoadFromFile(string assemblyPath)
     {
         var rawAssembly = File.ReadAllBytes(assemblyPath);
-
-        var pdbPath = Path.ChangeExtension(assemblyPath, "pdb");
-        if (File.Exists(pdbPath))
-        {
-            return Assembly.Load(rawAssembly, File.ReadAllBytes(pdbPath));
-        }
-
-        var mdbPath = Path.ChangeExtension(assemblyPath, "mdb");
-        if (File.Exists(mdbPath))
-        {
-            return Assembly.Load(rawAssembly, File.ReadAllBytes(mdbPath));
-        }
-
         return Assembly.Load(rawAssembly);
     }
 }
