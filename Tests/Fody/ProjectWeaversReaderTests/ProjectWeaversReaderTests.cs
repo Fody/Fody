8f66e64e2a6046cbd5cf4215b1303bbda4c17cf6
Simon Cropp <simon.cropp@gmail.com>
2019-04-06 07:50:49 +1100

    simplify test

--- Tests/Fody/ProjectWeaversReaderTests/ProjectWeaversReaderTests.cs
+++ Tests/Fody/ProjectWeaversReaderTests/ProjectWeaversReaderTests.cs
@@ -1,4 +1,3 @@
-using System.IO;
 using ApprovalTests;
 using Fody;
 using Xunit;
@@ -8,10 +7,9 @@ public class ProjectWeaversReaderTests : TestBase
     [Fact]
     public void Invalid()
     {
-        var currentDirectory = AssemblyLocation.CurrentDirectory;
-        var path = Path.Combine(currentDirectory, @"Fody\ProjectWeaversReaderTests\Invalid.txt");
+        var path =  @"Fody\ProjectWeaversReaderTests\Invalid.txt";
 
         var exception = Assert.Throws<WeavingException>(() => XDocumentEx.Load(path));
-        Approvals.Verify(exception.Message.Replace(currentDirectory, ""));
+        Approvals.Verify(exception.Message);
     }
 }
