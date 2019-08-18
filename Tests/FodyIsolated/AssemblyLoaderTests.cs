76a7bc9c4bd6b7aa1643ebafbc110c735f9b37b9
Simon Cropp <simon.cropp@gmail.com>
2019-08-18 19:31:57 +1000

    throw for incorrect fody version
    related to #734

---
+++ Tests/FodyIsolated/AssemblyLoaderTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+public class AssemblyLoaderTests :
+    XunitLoggingBase
+{
+    [Fact]
+    public void TestsShouldReferenceTheCorrectVersionOfFodyHelpers()
+    {
+        Assert.Equal("FodyHelpers", InnerWeaver.FindFodyHelpersReference(GetType().Assembly).Name);
+        InnerWeaver.ValidateWeaverReferencesAtLeastTheCurrentMajorOfFody(GetType().Assembly);
+    }
+
+    public AssemblyLoaderTests(ITestOutputHelper output) :
+        base(output)
+    {
+    }
+}
