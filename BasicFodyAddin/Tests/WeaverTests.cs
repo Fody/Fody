a5d73349c32927362924b7e46dac8b5026a27d3c
Simon Cropp <simon.cropp@gmail.com>
2019-11-16 09:45:22 +1100

    GitHubSync update

---
+++ BasicFodyAddin/Tests/WeaverTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Fody;
+using Xunit;
+
+#region WeaverTests
+
+public class WeaverTests
+{
+    static TestResult testResult;
+
+    static WeaverTests()
+    {
+        var weavingTask = new ModuleWeaver();
+        testResult = weavingTask.ExecuteTestRun("AssemblyToProcess.dll");
+    }
+
+    [Fact]
+    public void ValidateHelloWorldIsInjected()
+    {
+        var type = testResult.Assembly.GetType("TheNamespace.Hello");
+        var instance = (dynamic)Activator.CreateInstance(type);
+
+        Assert.Equal("Hello World", instance.World());
+    }
+}
+
+#endregion
---
+++ src/Docs/DocoUpdater.cs
@@ -0,0 +1,10 @@
+﻿using Xunit.Abstractions;
+
+public class DocoUpdater :
+    XunitApprovalBase
+{
+    public DocoUpdater(ITestOutputHelper output) :
+        base(output)
+    {
+    }
+}
