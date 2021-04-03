fb435b9b5e75936c0aa0d87a59f4cc96029f3f53
tom-englert <mail@tom-englert.de>
2021-04-03 12:30:37 +0200

    Add some log output.

--- FodyIsolated/StrongNameKeyFinder.cs
+++ FodyIsolated/StrongNameKeyFinder.cs
@@ -34,24 +34,27 @@ public partial class InnerWeaver
             {
                 try
                 {
+                    Logger.LogDebug("Extract public key from key file for signing.");
+
                     StrongNameKeyPair = new StrongNameKeyPair(fileBytes);
                     // Ensure that we can generate the public key from the key file. This requires the private key to
                     // work. If we cannot generate the public key, an ArgumentException will be thrown. In this case,
-                    // the assembly is delay-signed with a public only keyfile.
+                    // the assembly is delay-signed with a public only key-file.
                     // Note: The NETSTANDARD implementation of StrongNameKeyPair.PublicKey does never throw here.
                     PublicKey = StrongNameKeyPair.PublicKey;
                     return;
-
                 }
                 catch (ArgumentException)
                 {
+                    Logger.LogWarning("Failed to extract public key from key file, fall back to delay-signing.");
                 }
             }
 
             // Fall back to delay signing, this was the original behavior, however that does not work in NETSTANDARD (s.a.)
+            Logger.LogDebug("Prepare public key for delay-signing.");
 
-            // We know that we cannot sign the assembly with this keyfile. Let's assume that it is a public
-            // only keyfile and pass along all the bytes.
+            // We know that we cannot sign the assembly with this key-file. Let's assume that it is a public
+            // only key-file and pass along all the bytes.
             StrongNameKeyPair = null;
             PublicKey = fileBytes;
         }
