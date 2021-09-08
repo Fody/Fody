214cdfb533e36be79dd420259d2a5016e9c7777a
Geert van Horrik <geert@catenalogic.com>
2021-09-08 09:07:11 +0200

    Log weaver name and version

---
+++ FodyIsolated/AssemblyExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+static class AssemblyExtensions
+{
+    public static string GetVersion(this Assembly assembly)
+    {
+        var informationalVersion = GetAssemblyAttribute<AssemblyInformationalVersionAttribute>(assembly);
+        if (informationalVersion != null)
+        {
+            return informationalVersion.InformationalVersion;
+        }
+
+        var version = assembly.GetName().Version;
+        if (version != null)
+        {
+            return version.ToString();
+        }
+
+        return "unknown";
+    }
+
+    private static TAttibute? GetAssemblyAttribute<TAttibute>(Assembly assembly)
+        where TAttibute : Attribute
+    {
+        var attibutes = assembly.GetCustomAttributes(typeof(TAttibute))?.ToArray() ?? Array.Empty<Attribute>();
+        return attibutes.Length > 0 ? attibutes[0] as TAttibute : null;
+    }
+}
