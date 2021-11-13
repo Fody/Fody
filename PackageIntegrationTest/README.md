8aab6db033e9e8073d8aa265546f45103f67e67c
tom-englert <mail@tom-englert.de>
2021-11-13 12:45:29 +0100

    Add cross platform integration tests

---
+++ PackageIntegrationTest/README.md
@@ -0,0 +1,5 @@
+# PackageIntegrationTest
+A test application to verify compatibility with different Visual Studio versions or cross-platform builds
+
+- download the fody nuget package to test into the ./nuget folder (must have a higher version than the latest release on nuget.org!)
+- build & test
---
+++ PackageIntegrationTest/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d4711722-e256-4ae0-ba9f-2e3bfd9de7a1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
