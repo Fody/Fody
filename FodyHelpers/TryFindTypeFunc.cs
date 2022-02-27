1de94ab92c1dd04f9366236e65190b60f77bcb35
tom-englert <mail@tom-englert.de>
2022-02-27 11:03:10 +0100

    Fix: build fails with latest VS (17.1) due to inconsistent nullability annotations

--- FodyHelpers/TryFindTypeFunc.cs
+++ FodyHelpers/TryFindTypeFunc.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 using Mono.Cecil;
 
 namespace Fody
 {
     [Obsolete("No longer required as BaseModuleWeaver.TryFindType has been replace with BaseModuleWeaver.TryFindTypeDefinition", false)]
-    public delegate bool TryFindTypeFunc(string typeName, [NotNullWhen(true)] out TypeDefinition? type);
+    public delegate bool TryFindTypeFunc(string typeName, out TypeDefinition? type);
 }
