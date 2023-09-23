266abc2221ce53d0dc7bb67648a59699bd5da665
Simon Cropp <simon.cropp@gmail.com>
2023-09-23 20:49:11 +1000

    .

--- FodyHelpers/BaseModuleWeaver.cs
+++ FodyHelpers/BaseModuleWeaver.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using System.Xml.Linq;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
 
--- FodyHelpers/GlobalUsings.cs
+++ FodyHelpers/GlobalUsings.cs
@@ -4,3 +4,4 @@ global using System;
 global using System.Collections.Generic;
 global using System.IO;
 global using System.Linq;
+global using System.Xml.Linq;
